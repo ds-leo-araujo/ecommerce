9cbe841483e7505c7cc6bc8d48baf7a7a0af6e02
Leo Bruno <leobruno.developer@gmail.com>
2018-01-26 22:10:38 -0300

    recuperar senha

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -18,7 +18,7 @@
 		public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
 		{
 			$config = array(
-							"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+							"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
 							"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 							"debug"         => false // set to false to improve the speed
 						   );
@@ -28,15 +28,23 @@
 			$tpl = new Tpl;
 
 			foreach ($data as $key => $value) {
-				$tpl->assign($key, $value)
+				$tpl->assign($key, $value);
 			}
 
-			$html = $tpl=>draw($tplName, true);
+			$html = $tpl->draw($tplName, true);
 
 			$this->mail = new \PHPMailer;
 
 			//Tell PHPMailer to use SMTP
 			$this->mail->isSMTP();
+
+			$this->mail->SMTPOptions = array(
+    			'ssl' => array(
+			        'verify_peer' => false,
+			        'verify_peer_name' => false,
+			        'allow_self_signed' => true
+    			)
+ 			);
 
 			//Enable SMTP debugging
 			// 0 = off (for production use)
