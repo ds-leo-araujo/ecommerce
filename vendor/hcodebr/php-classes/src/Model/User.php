b022438e7e1f0ca8cfb22ecec3ec4f8fcc2c396b
Leo Bruno <leobruno.developer@gmail.com>
2018-04-22 00:02:46 -0400

    Site Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -63,8 +63,8 @@
 		{
 			$sql = new Sql();
 
-			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-				":LOGIN"=>$login
+			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
+			 	":LOGIN"=>$login
 			));
 
 			if (count($results) === 0)
