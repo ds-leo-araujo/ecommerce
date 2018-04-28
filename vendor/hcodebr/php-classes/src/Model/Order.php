55b492788fa6a9d9ec5ce70ac79e7ec7efe832d4
Leo Bruno <leobruno.developer@gmail.com>
2018-04-28 11:53:53 -0400

    Admin - Pedidos

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -8,10 +8,15 @@
 
 	use \Hcode\DB\Sql;
 	use \Hcode\Model;
+	use \Hcode\Model\Cart;
+
 
 	class Order extends Model
 	{
-		
+
+		const SUCCESS = "Order-Success";
+		const ERROR = "Order-Error";
+
 		public function save()
 		{
 			$sql = new Sql();
@@ -52,6 +57,115 @@
 			{
 				$this->setData($results[0]);
 			}
+		}
+
+		public static function listAll()
+		{
+			$sql = new Sql();
+
+			return $sql->select("
+				SELECT * 
+				FROM tb_orders a 
+				INNER JOIN tb_ordersstatus b USING(idstatus)
+				INNER JOIN tb_carts c USING(idcart)
+				INNER JOIN tb_users d ON d.iduser = a.iduser
+				INNER JOIN tb_addresses e USING(idaddress)
+				INNER JOIN tb_persons f ON f.idperson = d.idperson
+				ORDER BY a.dtregister DESC
+			");
+		}
+
+		public function delete()
+		{
+			$sql = new Sql();
+
+			$sql->query("DELETE FROM tb_orders WHERE idorder = :idorder", [
+				':idorder'=>$this->getidorder()
+			]);
+		}
+
+		public function getCart():Cart
+		{
+			$cart = new Cart();
+
+			$cart->get((int)$this->getidcart());
+
+			return $cart;
+		}
+
+		public static function setError($msg)
+		{
+			$_SESSION[Order::ERROR] = $msg;
+		}
+
+		public static function getError()
+		{
+			$msg = (isset($_SESSION[Order::ERROR]) && $_SESSION[Order::ERROR]) ? $_SESSION[Order::ERROR] : '';
+
+			Order::clearError();
+
+			return $msg;
+		}
+
+		public static function clearError()
+		{
+			$_SESSION[Order::ERROR] = NULL;
+		}
+
+		public static function setErrorRegister($msg)
+		{
+			$_SESSION[Order::ERROR_REGISTER] = $msg;
+		}
+
+		public static function getErrorRegister()
+		{
+			$msg = (isset($_SESSION[Order::ERROR_REGISTER]) && $_SESSION[Order::ERROR_REGISTER]) ? $_SESSION[Order::ERROR_REGISTER] : '';
+
+			Order::clearErrorRegister();
+
+			return $msg;
+		}
+
+		public static function clearErrorRegister()
+		{
+			$_SESSION[Order::ERROR_REGISTER] = NULL;
+		}
+
+		public static function checkLoginExist($login)
+		{
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+				':deslogin'=>$login
+			]);
+
+			return (count($results) > 0);
+		}
+
+		public static function setSuccess($msg)
+		{
+			$_SESSION[Order::SUCCESS] = $msg;
+		}
+
+		public static function getSuccess()
+		{
+			$msg = (isset($_SESSION[Order::SUCCESS]) && $_SESSION[Order::SUCCESS]) ? $_SESSION[Order::SUCCESS] : '';
+
+			Order::clearSuccess();
+
+			return $msg;
+		}
+
+		public static function clearSuccess()
+		{
+			$_SESSION[Order::SUCCESS] = NULL;
+		}
+
+		public static function getPasswordHash($password)
+		{
+			return password_hash($password, PASSWORD_DEFAULT, [
+				'cost'=>12
+			]);
 		}
 	}
 ?>
